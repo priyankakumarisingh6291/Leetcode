c59a67cc174d932f90f59b8787b379810ce456fc
priyankakumarisingh6291 <priyankakumarisingh6291@gmail.com>
2026-08-23 19:29:46 +0530

    Time: 0 ms (100%), Space: 43.7 MB (71.42%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] mat) {
+
+        int n = mat.length;
+
+        // Transpose
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+
+            while (left < right) {
+                int temp = mat[i][left];
+                mat[i][left] = mat[i][right];
+                mat[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+    }
+}
